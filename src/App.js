d80b681a1f96d817beb95f89702d89f3c3a69320
Naveen-1401 <naveen.verma@cogoport.com>
2023-01-06 09:26:51 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect} from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+import useBooksContext from './hooks/use-books-context';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    
+    const {fetchBooks }  = useBooksContext();
+
+    useEffect(() => {
+        fetchBooks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return <div className = "app"> 
+        <h1> Reading List </h1>
+        <BookCreate />
+        <BookList />
+    </div>;
 }
-
 export default App;
